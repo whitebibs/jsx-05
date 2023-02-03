30c18f5ef2117184dfc01d6214ee07b3a640e610
whitebibs <115084982+whitebibs@users.noreply.github.com>
2023-02-03 12:18:28 +0100

    Add files via upload

---
+++ src/Sum.js
@@ -0,0 +1,2 @@
+const sum= (a,b) => a+b
+export const result= <h2>The result is{sum(10,2)}</h2>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Sum } from "./Sum";
+import ReactDOM from "react-dom"
+
+const root= document.querySelector("#root");
+
+ReactDOM.render(< Sum/>,root)
+
